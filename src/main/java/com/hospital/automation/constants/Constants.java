898eb3eb2408e078298388b0505e296c878480f1
Sai Durga Pradeep Veeramalla <vsaidurgapradeep2003@gmail.com>
2026-04-16 23:15:01 +0530

    Add Constants.java file with predefined constants

---
+++ src/main/java/com/hospital/automation/constants/Constants.java
@@ -0,0 +1,17 @@
+package com.hospital.automation.constants;
+
+public class Constants {
+    public static final String BASE_URL = "https://www.practo.com/";
+    public static final String BANGALORE_CITY = "Bangalore";
+    public static final double MIN_RATING = 3.5;
+    public static final String AVAILABILITY_24_7 = "Available 24/7";
+    public static final int EXPLICIT_WAIT = 15;
+    public static final int IMPLICIT_WAIT = 10;
+    public static final int PAGE_LOAD_TIMEOUT = 30;
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String EDGE = "edge";
+    public static final String REPORTS_PATH = System.getProperty("user.dir") + "/reports/";
+    public static final String SCREENSHOTS_PATH = System.getProperty("user.dir") + "/screenshots/";
+    public static final String TEST_DATA_PATH = System.getProperty("user.dir") + "/src/test/resources/testdata/";
+}
